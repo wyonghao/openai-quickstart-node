b9fd0384f7f0eb74963953f00cc13b4457279a2f
leo_yw <wyonghao@outlook.com>
2022-12-10 21:18:01 +0000

    add "max_tokens" to 550

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,6 +13,7 @@ export default async function (req, res) {
     model: "text-davinci-003",
     prompt: generatePrompt(req.body),
     temperature: 0.6,
+    max_tokens: 550,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
